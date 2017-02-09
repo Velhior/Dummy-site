fb9dfa10a41f9f4b6fd0570d9c2ef96bf34d40ee
Vladimir Mironsky <phlegmatic.dev@gmail.com>
2017-02-09 09:45:07 +0300

    Added some frontend stuff(Main layout, main page, contact page)

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PagesController@getIndex');
+Route::get('/contact', 'PagesController@getContact');
 
 
 Route::group(['prefix' => 'admin'], function () {
